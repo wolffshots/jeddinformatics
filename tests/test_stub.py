bbbe27c268825ea3dcf259faee3a18e984399a99
jadon <16850875+wolffshots@users.noreply.github.com>
2024-03-15 02:27:45 +0200

    chore: add stub test

---
+++ tests/test_stub.py
@@ -0,0 +1,8 @@
+import unittest
+
+class TestStub(unittest.TestCase):
+    def test_always_true(self):
+        self.assertTrue(True)
+
+    def test_always_false(self):
+        self.assertFalse(False)
